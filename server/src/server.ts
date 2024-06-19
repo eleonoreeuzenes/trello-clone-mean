54b0543271fa75f3ff6cfb39704e0ce223c924e0
Eleonore Euzenes <eleonore.euzenes@gmail.com>
2024-06-19 18:35:13 +0200

    setting up server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,25 @@
-console.log("server");
+import express from 'express';
+import { createServer } from 'http';
+import {Server} from 'socket.io';
+import mongoose from 'mongoose';
+
+const app = express();
+const httpServer = createServer(app);
+const io= new Server(httpServer);
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+io.on('connection', () => {   
+    console.log('socket connect');
+});
+
+mongoose.connect('mongodb://localhost:27017/trello').then(() => {
+    console.log('Connected to MongoDB');
+    httpServer.listen(4001, () => {
+        console.log('listening on *:4001');
+    });
+}).catch((error) => {
+    console.log('Error connecting to MongoDB', error);
+});
