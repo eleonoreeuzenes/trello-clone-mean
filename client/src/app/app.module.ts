080760001cc3c9fcb71dc41554f4d7dd537c57f0
Eleonore Euzenes <eleonore.euzenes@gmail.com>
2024-06-20 14:15:20 +0200

    homepage

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, AuthModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, AuthModule, HttpClientModule, HomeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
