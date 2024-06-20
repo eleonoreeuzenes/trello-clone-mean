6424fc19f22b2bb9d5315b864d732f9644dc24ca
Eleonore Euzenes <eleonore.euzenes@gmail.com>
2024-06-20 11:41:30 +0200

    create auth module

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './auth/services/auth.service';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authService.getCurrentUser().subscribe({
+      next: (res) => {
+        console.log('res', res);
+      },
+      error: (err) => {
+        console.log('err', err);
+        this.authService.setCurrentUser(null);
+      },
+    });
+  }
+}
