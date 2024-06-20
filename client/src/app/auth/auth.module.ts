8b0aad869d3d8468d2e44aaa9b7c99eee45a0720
Eleonore Euzenes <eleonore.euzenes@gmail.com>
2024-06-20 13:32:15 +0200

    register service and validation

--- client/src/app/auth/auth.module.ts
+++ client/src/app/auth/auth.module.ts
@@ -3,6 +3,7 @@ import {RouterModule, Routes} from "@angular/router";
 import {AuthService} from "./services/auth.service";
 import { RegisterComponent } from "./components/register/register.component";
 import { ReactiveFormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
 
 
 const routes: Routes = [
@@ -13,7 +14,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes), ReactiveFormsModule],
+  imports: [RouterModule.forChild(routes), ReactiveFormsModule, CommonModule],
   providers: [AuthService],
   declarations: [RegisterComponent],
 })
--- client/src/app/auth/components/register/register.component.ts
+++ client/src/app/auth/components/register/register.component.ts
@@ -1,21 +1,39 @@
 import { Component } from "@angular/core";
 import { FormBuilder, Validators } from "@angular/forms";
+import { AuthService } from "../../services/auth.service";
+import { HttpErrorResponse } from "@angular/common/http";
+import { Router } from '@angular/router';
 
 @Component({
   selector:'auth-register',
   templateUrl:'./register.component.html'
 })
 export class RegisterComponent {
+  errorMessage: string | null = null;
   form = this.fb.group({
     email: ['', Validators.required],
     username: ['', Validators.required],
     password: ['', Validators.required]
   })
 
-  constructor(private fb: FormBuilder) { }
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   onSubmit(): void {
-    console.log('value', this.form.value);
+    this.authService.register(this.form.value).subscribe({
+      next: (currentUser) => {
+        this.authService.setToken(currentUser)
+        this.authService.setCurrentUser(currentUser)
+        this.errorMessage = null;
+        this.router.navigateByUrl('/');
+      },
+      error: (err: HttpErrorResponse) => {
+        console.log('err', err.error);
+        this.errorMessage = err.error.join(', ');
+      }
+    })
   }
-
 }
