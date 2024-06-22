3bdc82a47b6904265fffe5bbf372156973c9a0b9
Eleonore Euzenes <eleonore.euzenes@gmail.com>
2024-06-22 10:18:05 +0200

    create boards on server side

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,29 +1,38 @@
 import express from 'express';
 import { createServer } from 'http';
-import {Server} from 'socket.io';
+import { Server } from 'socket.io';
 import mongoose from 'mongoose';
 import * as usersController from './controllers/users';
+import * as boardsController from './controllers/boards';
 import bodyParser from 'body-parser';
 import authMiddleware from './middlewares/auth';
 import cors from 'cors';
 
 const app = express();
 const httpServer = createServer(app);
-const io= new Server(httpServer);
+const io = new Server(httpServer);
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+    res.send('Hello World');
 });
 
 app.post('/api/register', usersController.register);
 app.post('/api/login', usersController.login);
 app.get('/api/user', authMiddleware, usersController.getCurrentUser);
+app.get('/api/boards', authMiddleware, boardsController.getBoards);
 
-io.on('connection', () => {   
+mongoose.set('toJSON', {
+    virtuals: true,
+    transform: (_, converted) => {
+        delete converted._id;
+    }
+});
+
+io.on('connection', () => {
     console.log('socket connect');
 });
 
